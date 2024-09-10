5ddb56ea583e5f2f66ecd217493bdd66ecea5455
Lennart <80266866+lennart-rth@users.noreply.github.com>
2024-09-10 14:03:29 +0200

    formatting and some more comments

--- excercise1/src/publisher/scripts/talker.py
+++ excercise1/src/publisher/scripts/talker.py
@@ -3,9 +3,12 @@
 import rospy
 from std_msgs.msg import UInt16
 
+
 def publisher(n, k):
-    pub = rospy.Publisher('roth', UInt16, queue_size = 10)       # Unit16 range is 0...65535. With 20hz we have 819sec until overflow.
-    rospy.init_node('publisher', anonymous = True)
+    pub = rospy.Publisher(
+        "roth", UInt16, queue_size=10
+    )  # UInt16 range is 0...65535. With 20hz we have 819sec (13,6min) until overflow. With UInt8 it is only 3 sec. Not feasable for this assignment.
+    rospy.init_node("publisher", anonymous=True)
     rate = rospy.Rate(20)
 
     while not rospy.is_shutdown():
@@ -18,9 +21,9 @@ def publisher(n, k):
         rate.sleep()
 
 
-if __name__ == '__main__':
-    n = 4
-    k = 0   # k is integer and k > 0
+if __name__ == "__main__":
+    n = 4  # the multiple used for iterating k  
+    k = 0  # k is integer and k > 0
     try:
         publisher(n, k)
     except rospy.ROSInterruptException:
--- excercise1/src/subscriber/scripts/listener.py
+++ excercise1/src/subscriber/scripts/listener.py
@@ -4,18 +4,21 @@ import rospy
 from std_msgs.msg import UInt16
 from std_msgs.msg import Float32
 
-def callback(data, pub, q):
-    rospy.loginfo(rospy.get_caller_id() + ': %s', data.data)
-    msg = data.data / q     # float now
-    pub.publish(msg)
-    
-def listener(q):
-    rospy.init_node('subscriber', anonymous=True)
-    pub = rospy.Publisher('/kthfs/result', Float32, queue_size=10)
 
-    rospy.Subscriber('roth', UInt16, lambda x: callback(x, pub, q))
+def callback(data, pub, q):
+    rospy.loginfo(rospy.get_caller_id() + ": %s", data.data)
+    msg = data.data / q  # float now
+    pub.publish(msg)
+
+
+def listener(q):
+    rospy.init_node("subscriber", anonymous=True)
+    pub = rospy.Publisher("/kthfs/result", Float32, queue_size=10)
+
+    rospy.Subscriber("roth", UInt16, lambda x: callback(x, pub, q))
     rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     q = 0.15
     listener(q)
